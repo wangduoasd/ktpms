245990a27cb92a37c15d8b2a6db6b38216f14780
suzehua <suzehua@kaituocn.com>
2018-08-07 11:23:16 +0800

    新版

---
+++ src/main/java/com/kaituo/pms/config/test.java
@@ -0,0 +1,11 @@
+package com.kaituo.pms.config;
+
+/**
+ * @program: pms
+ * @description:
+ * @author: su
+ * @create: 2018-08-07 11:21
+ **/
+
+public class test {
+}
---
+++ src/main/java/com/kaituo/pms/controller/test.java
@@ -0,0 +1,11 @@
+package com.kaituo.pms.controller;
+
+/**
+ * @program: pms
+ * @description:
+ * @author: su
+ * @create: 2018-08-07 11:22
+ **/
+
+public class test {
+}
