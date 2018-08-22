bd5b50627d1fffbf517c357af5c600580d86ca60
Administrator <1747002392@qq.com>
2018-08-22 10:17:49 +0800

    新的Exchange

--- src/main/java/com/kaituo/pms/dao/ExchangeMapper.java
+++ src/main/java/com/kaituo/pms/dao/ExchangeMapper.java
@@ -3,8 +3,12 @@ package com.kaituo.pms.dao;
 import com.kaituo.pms.bean.Exchange;
 import com.kaituo.pms.bean.ExchangeExample;
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+@Mapper
+@Repository
 public interface ExchangeMapper {
     int countByExample(ExchangeExample example);
 
@@ -27,4 +31,12 @@ public interface ExchangeMapper {
     int updateByPrimaryKeySelective(Exchange record);
 
     int updateByPrimaryKey(Exchange record);
+
+    List<Exchange> findExchangeRecord(@Param("userId") int userId);
+    List<Exchange> selectBykeyWord(@Param("keyWord") String keyWord,@Param("userId")int userId);
+
+    List<Exchange> getExchangeLists();
+    List<Exchange> selectBykeyWord2(@Param("keyWord") String keyWord);
+    String toUserName(@Param("userId") int userId);
+    String getPrizeImage(@Param("prizeId") int prizeId);
 }
